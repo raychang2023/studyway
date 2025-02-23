f80d223904a24bf11b4ca65864593d4e2defa6cb
Wei Zheng <70025059@qq.com>
2025-02-23 11:08:45 +0800

    更新: 修改CORS配置的允许头部

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ app.add_middleware(
     ],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["Content-Type", "Accept", "Authorization", "X-Requested-With"],
 )
 
 class Topic(BaseModel):
